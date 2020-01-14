64f8bf8e63740ffa5b5b8f5d756b3ef6f2c28e87
Tyler Shuck <tshuck88@gmail.com>
2020-01-14 15:02:14 -0800

    added a saved boolean to the article model. created main and index handlebar layouts

--- models/Article.js
+++ models/Article.js
@@ -6,12 +6,12 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
+
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
+
   link: {
     type: String,
     required: true
@@ -19,9 +19,11 @@ var ArticleSchema = new Schema({
   summary: {
       type: String,
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
+  saved: {
+      type: Boolean,
+      required: true,
+      default: false
+  },
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
